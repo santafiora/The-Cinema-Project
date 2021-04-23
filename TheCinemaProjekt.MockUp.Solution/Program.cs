de4c50ca1b9b2d13e19051e6131dfba86e90fce3
ChessSuite <39228929+santafiora@users.noreply.github.com>
2021-04-23 15:16:40 +0200

    MockupTest
    MockupTest

---
+++ TheCinemaProjekt.MockUp.Solution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCinemaProjekt.MockUp.Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
